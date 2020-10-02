0e921bc3fd6de4d909a75b3ed8cdf5a2f0700aa5
ArondevIta <aronamaral@gmail.com>
2020-10-01 23:56:15 -0300

    add text in main

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+function Main() {
+  return (
+    <section className="main">
+      <p>BOLA DE FUTSAL MAX 1000</p>
+    </section>
+  );
+}
+
+export default Main;
